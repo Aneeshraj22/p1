061c69735adc5c90013e8752af4566b6c0d9e7ef
username <username@cognizant.com>
2022-08-19 09:52:18 +0530

    Add project files.

---
+++ p1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("enter no");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter no1");
+            b =Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
