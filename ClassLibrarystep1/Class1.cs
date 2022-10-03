8421779058b10fe06ef6d45f09fb561eebdedb21
username <username@cognizant.com>
2022-10-03 20:40:22 +0530

    new1

--- ClassLibrarystep1/Class1.cs
+++ ClassLibrarystep1/Class1.cs
@@ -4,13 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace p1
+namespace ClassLibrarystep1
 {
-    internal class Program
+    public class Class1
     {
-        static void Main(string[] args)
-        {
-
-        }
     }
 }
--- p1/Class1.cs
+++ p1/Class1.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace p1
 {
-    internal class Program
+    internal class Class1
     {
-        static void Main(string[] args)
-        {
-
-        }
     }
 }
